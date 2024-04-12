b96fe47abe82d42fd4c9439c18a6f6eae2d6645d
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-04-12 11:50:26 -0600

    Add creation relationship route

--- routes/create.py
+++ routes/create.py
@@ -1,7 +1,7 @@
 from typing import List, Dict, Any
 
 from fastapi import APIRouter, HTTPException
-from tools import node, basicResponse
+from tools import node, basicResponse, createRelationship, NodeD, relationship
 
 from tools import createNode
 
@@ -32,6 +32,21 @@ async def create_nodes(Ns: List[node]):
 
             createNode(labels, properties, merge)
         response_data = {'status': f'success to create {len(Ns)} nodes'}
+        return basicResponse(**response_data)
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+
+@create.post('/relationship', response_model=basicResponse, response_model_exclude_unset=True)
+async def create_relationship(R: relationship):
+    try:
+        properties: Dict[str, Any] = R.properties
+        type_r: str = R.type
+        node1: node = R.node1
+        node2: node = R.node2
+
+        createRelationship(typeR=type_r, properties=properties, node1=NodeD(node1.labels, node1.properties), node2=NodeD(node2.labels, node2.properties))
+        response_data = {'status': 'success to create relationship'}
         return basicResponse(**response_data)
     except Exception as e:
         return HTTPException(status_code=500, detail=str(e))

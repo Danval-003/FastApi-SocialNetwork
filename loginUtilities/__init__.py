075e0372cbc5d316e9f34016e4bf6642d564c9e6
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-04-13 10:34:29 -0600

    upddate token use

--- loginUtilities/__init__.py
+++ loginUtilities/__init__.py
@@ -17,25 +17,28 @@ def get_user(email: str):
 # Decorador para requerir autenticación
 def authenticate_required(func):
     async def wrapper(request: Request):
-        token = await oauth2_scheme.__call__(request)
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-            email: str = payload.get("sub")
-            if email is None:
+            token = await oauth2_scheme.__call__(request)
+            try:
+                payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+                email: str = payload.get("sub")
+                if email is None:
+                    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+                user = get_user(email)
+                if not user:
+                    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+            except jwt.ExpiredSignatureError:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+            except jwt.PyJWTError:
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-            user = get_user(email)
-            if not user:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-        except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
-        except jwt.PyJWTError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
-        # Almacenar el objeto de usuario autenticado en la solicitud
-        request.state.user = user
+            # Almacenar el objeto de usuario autenticado en la solicitud
+            request.state.user = user
 
-        # Llamar a la función decorada con el objeto de usuario autenticado
-        return await func(request)
+            # Llamar a la función decorada con el objeto de usuario autenticado
+            return await func(request)
+        except Exception as e:
+            return HTTPException(status_code=500, detail=str(e))
 
     return wrapper
 

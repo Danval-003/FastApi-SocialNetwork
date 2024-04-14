5f802fa83a5e95e65bb59d4d5215fa70d07b66cf
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-04-13 20:44:37 -0600

    Finish to config login options

--- routes/read.py
+++ routes/read.py
@@ -63,8 +63,8 @@ async def searchAffiliate(request: Request):
     try:
         userId = request.state.user.properties['userId']
         print("User ID: ", userId)
-        query = f"MATCH (u:User:Person {{'userId': {userId}}})-[r:AFFILIATE]-(a:User:Organization) RETURN r"
-
+        query = f"MATCH (u:User:Person {{'userId': '{userId}'}})-[r:AFFILIATE]-(a:User:Organization) RETURN r"
+        print(query)
         results = makeQuery(query, listOffIndexes=['r'])
 
         if len(results) == 0:
